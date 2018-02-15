782f8f8796a26a50bc2e6f832d716a7c3ddd2815
andrezao12 <35736027+andrezao12@users.noreply.github.com>
2018-02-15 16:52:54 -0200

    Add files via upload

--- saida_insert.php
+++ saida_insert.php
@@ -11,15 +11,18 @@ mysqli_query($conn, $update);
  $query = $conn->query("SELECT * FROM entrada WHERE id = ".$_POST['placa']." ");
 // Attempt insert query execution
 $row = $query->fetch_assoc();
+$data_s = strtotime($row['data_hora_saida']);
+$data_e = strtotime($row['data_hora_entrada']);
 
-$tempo = (new DateTime($row['data_hora_saida']))->diff(new DateTime($row['data_hora_entrada']))->format('%H%I%S');
+$tempo = $data_s - $data_e;
 
+$dias = floor($tempo / 86400);
+$horas   = floor(($tempo - ($dias * 86400)) / 3600) + $dias*24;
+$minutos = floor(($tempo - ($dias * 86400) - ($horas * 3600))/60) + $horas*60;
+$seconds = "00";
+
+$tempo = $horas.$minutos.$seconds;
 $sql = "UPDATE entrada SET tempo = '".$tempo."' WHERE id = ".$_POST['placa']." ";
-
-$min_seg = $tempo % 10000;
-$segundos = $tempo % 100;
-$minutos = ($min_seg - $segundos)/100;
-$horas = ($tempo - $min_seg)/10000;
 
 $valor = 0;
 
@@ -27,7 +30,7 @@ if($horas < 1)
 {
 	if($minutos < 41)
 			$valor = 3;
-	
+
 	else
 		$valor = 6;
 }
@@ -47,17 +50,17 @@ mysqli_query($conn, $total);
 $status ="UPDATE entrada SET status = 1 WHERE id = ".$_POST['placa']." ";
 mysqli_query($conn, $status);
 
-							 
+
 if(mysqli_query($conn, $sql)){
     echo "<script type='text/javascript'>
-	alert('Saída registrada com sucesso'); 
-	document.location.href='saida.php';
+	alert('Saída registrada com sucesso');
+	//document.location.href='saida.php';
 </script>";	// MUDAR PARA document.location.href='/' QUANDO MIGRAR PRO SERVIDOR
 
 } else{
     echo "<script type='text/javascript'>alert('ERROR: Não foi possível registrar $sql. ')</script>" . mysqli_error($conn);
 }
- 
+
 // Close connection
 mysqli_close($conn);
 ?>
