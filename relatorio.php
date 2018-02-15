bb957230fad25228f700e5760bba46e2847f17d9
Joaomsv <joaomarcossv@gmail.com>
2018-02-15 17:30:09 -0200

    Revisão cosmetico: relatorio

--- relatorio.php
+++ relatorio.php
@@ -11,13 +11,13 @@
 	}
  ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
-	<title>Contact V17</title>
+	<title>Relatório</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<link rel="icon" type="image/png" href="images/icons/relicon.png"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
