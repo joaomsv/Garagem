ae5615365b2b402fb22101660ebc56436b2c93d8
Joaomsv <joaomarcossv@gmail.com>
2018-03-08 17:08:01 -0300

    add user icon

--- remover.php
+++ remover.php
@@ -17,7 +17,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="images/icons/caricon.png"/>
+	<link rel="icon" type="image/png" href="images/icons/usericon.png"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
